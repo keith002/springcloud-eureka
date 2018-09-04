5b02a464b07e4154a721bd59bf0f0c31d847f8d3
keith002 <keith002@sina.com>
2018-09-04 13:51:34 +0800

    https://github.com/keith002/springcloud-eureka.git
    SpringCloud：https://blog.csdn.net/Keith003/article/details/82382245

--- eureka-colony/src/main/java/com/csdn/EurekaColonyApplication.java
+++ eureka-colony/src/main/java/com/csdn/EurekaColonyApplication.java
@@ -1,13 +1,17 @@
 package com.csdn;
 
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaColonyApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -17,17 +21,17 @@ public class EurekaColonyApplication extends SpringBootServletInitializer {
 	/**
 	 * 打jar 进行使用
 	 */
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
 //		SpringApplication.run(EurekaColonyApplication.class, args);
-	    new SpringApplicationBuilder(EurekaColonyApplication.class).run(args);
-	}
+//	    new SpringApplicationBuilder(EurekaColonyApplication.class).run(args);
+//	}
 	/**
 	 * 本地测试使用
 	 */
-//	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//		String profiles = scanner.nextLine();
-//		new SpringApplicationBuilder(EurekaColonyApplication.class).profiles(profiles).run(args);
-//	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String profiles = scanner.nextLine();
+		new SpringApplicationBuilder(EurekaColonyApplication.class).profiles(profiles).run(args);
+	}
 
 }
